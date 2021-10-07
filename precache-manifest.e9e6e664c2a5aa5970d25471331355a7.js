dcc738a8763d820ff676b1fb10a65bfc91c6a8e7
yogesh <yogesh1996gupta@gmail.com>
2021-10-07 22:44:04 +0530

    Updates

---
+++ precache-manifest.e9e6e664c2a5aa5970d25471331355a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "587996ada5c26db57f9c4fa2c207476e",
+    "url": "/pizzaareactwebsite/index.html"
+  },
+  {
+    "revision": "7fade588f26399e4d62c",
+    "url": "/pizzaareactwebsite/static/css/main.e3a27a06.chunk.css"
+  },
+  {
+    "revision": "82f39457559270b99e80",
+    "url": "/pizzaareactwebsite/static/js/2.38dfb4cf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pizzaareactwebsite/static/js/2.38dfb4cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7fade588f26399e4d62c",
+    "url": "/pizzaareactwebsite/static/js/main.0c46e400.chunk.js"
+  },
+  {
+    "revision": "d7a456515670c6ffaa4d",
+    "url": "/pizzaareactwebsite/static/js/runtime-main.51dd3a67.js"
+  }
+]);
